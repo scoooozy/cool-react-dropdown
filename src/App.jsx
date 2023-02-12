50243cfd0b4dc12ffcede05ae8949457f01bd39e
Scoozy <79184720+scoooozy@users.noreply.github.com>
2023-02-12 12:09:43 -0800

    done w fetch request i think

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+import EventData from "./components/EventData";
+const App = () => {
+  return <div>
+    <EventData eventCode="2022cave" apiKey="Mtj24QizuCWNJkwlGJ7fkOyk93ahxvaZ2oGqMcqe34yYekfUScGslRrbgVja4ghu" />
+  </div>;
+};
+
+export default App;
