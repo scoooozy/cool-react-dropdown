f22b5fcdab54276f2b7222be01d54e25255cb6e9
Scoozy <79184720+scoooozy@users.noreply.github.com>
2023-02-26 13:49:47 -0800

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
 import React from "react";
 import "./App.css";
-import EventData from "./components/EventData";
-const App = () => {
-  return <div>
-    <EventData eventCode="2022cave" apiKey="Mtj24QizuCWNJkwlGJ7fkOyk93ahxvaZ2oGqMcqe34yYekfUScGslRrbgVja4ghu" />
-  </div>;
+import { useState } from "react";
+
+
+const Dropdown = ({ options }) => {
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="dropdown">
+      <div className={`select ${isOpen ? 'select-clicked' : ''}`} onClick={handleClick}>
+        <span className="selected">{selectedOption}</span>
+        <div className={`caret ${isOpen ? 'caret-rotate' : ''}`}></div>
+      </div>
+      <ul className={`menu ${isOpen ? 'menu-open' : ''}`}>
+        {options.map((option) => (
+          <li
+            key={option}
+            className={option === selectedOption ? "active" : ""}
+            onClick={() => handleOptionClick(option)}
+          >
+            {option}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
-export default App;
+export default function App() {
+  const options = ["item 1", "item 2", "item 3", "item 4", "item 5"];
+
+  return (
+    <div className="App">
+      <Dropdown options={options} />
+    </div>
+  );
+}
+
+
